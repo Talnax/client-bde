9a73b95e6428e5276a74fc73c86e07e5bde928ed
Rost Talnax <rosttalnax@Rosts-MacBook-Pro.local>
2020-01-26 21:44:03 -0800

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <v-app>
+
+    <!--
     <v-app-bar
       app
       color="primary"
-      dark
-    >
+      dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -12,48 +13,88 @@
           contain
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
           transition="scale-transition"
-          width="40"/>
-
+          width="40"></v-img>
         <v-img
           alt="Vuetify Name"
           class="shrink mt-1 hidden-sm-and-down"
           contain
           min-width="100"
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"/>
-
+          width="100"></v-img>
       </div>
-
       <v-spacer></v-spacer>
-
       <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
-        text
-      >
+        text>
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
+    -->
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <div class="vl"></div>
+    <div class="middle-block"></div>
+    <div class="right-block"></div>
+
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+
 
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
 
   data: () => ({
     //
   }),
 };
 </script>
+
+<!--<style lang="sass">-->
+<!--vertical line-->
+<!--https://stackoverflow.com/questions/3148415/how-to-make-a-vertical-line-in-html-->
+<style>
+
+  .vl {
+    border-left: 2px solid gray;
+    margin-left: 20px;
+    height: 90%;
+    margin-top: 2%;
+  }
+
+  .middle-block {
+    width: 20px;
+    height: 2px;
+    top: 100px;
+    left: 11px;
+    border: 1px solid gray;
+    position: absolute;
+  }
+
+  .right-block {
+    width: 20px;
+    height: 6px;
+    top: 98px;
+    left: 60px;
+    border: 3px solid gray;
+    position: absolute;
+  }
+
+  .left-block {
+    width: 100px;
+    height: 20px;
+    top: 10px;
+    left: 10px;
+    border:1px solid #777;
+    position: absolute;
+  }
+
+
+
+  .gl1 {
+
+  }
+
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,5 +3,42 @@ import Vuetify from 'vuetify/lib';
 
 Vue.use(Vuetify);
 
+/*
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.red.darken1, // #E53935
+        secondary: colors.red.lighten4, // #FFCDD2
+        accent: colors.indigo.base, // #3F51B5
+      },
+    },
+  },
+})
+*/
+
+export default new Vuetify({
+    theme: {
+        dark: true,
+        themes: {
+            dark: {
+                primary: '#21CFF3',
+                accent: '#FF4081',
+                secondary: '#FFE18D',
+                success: '#4CAF50',
+                info: '#2196F3',
+                warning: '#FB8C00',
+                error: '#FF5252'
+            },
+            light: {
+                primary: '#1976D2',
+                accent: '#e91e63',
+                secondary: '#30B1DC',
+                success: '#4CAF50',
+                info: '#2196F3',
+                warning: '#FB8C00',
+                error: '#FF5252'
+            }
+        }
+    }
 });
